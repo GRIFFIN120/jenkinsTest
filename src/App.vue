d22ffd3ae2bf41c0e381487dc4640cb11c034855
GRIFFIN120 <78230696@qq.com>
2020-07-12 20:10:00 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">主页</router-link> |
-      <router-link to="/about">关1于</router-link>
+      <router-link to="/about">关12于</router-link>
     </div>
     <router-view/>
   </div>
